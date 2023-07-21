dc943b0981ca4ba32d76d83eeb7d71f89986db2c
alexdeathway <alexdeathway@gmail.com>
2023-07-21 20:00:30 +0530

    [API] users registration and token

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from api import views
+
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 app_name = 'api'
 app_version='v1.0.0'
 
+
 urlpatterns = [
-    
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
+    path('test/', views.testEndPoint, name='test'),
+    path('docs/', views.DocsView.as_view(), name='docs'),
+    path('', views.getRoutes,name='routes')
 ]
