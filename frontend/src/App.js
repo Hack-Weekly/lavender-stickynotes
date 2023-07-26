e3bfdc5040107f635d2d0f3a9de59a335739af87
Tbone <timwallacedev@gmail.com>
2023-07-26 17:38:57 -0400

    created landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,31 @@ import { FormPage } from "./pages/FormPage";
 import { useEffect, useState } from "react";
 import { isAuth } from "./services/utils/isAuth";
 import { logout } from "./services/endpoints/users";
+import { LandingPage } from "./pages/LandingPage"
+
 export default function App() {
   const location = useLocation();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(()=>{
-    const checkAuth = async() => {
-      try{
-      setIsAuthenticated(await isAuth());
-      }catch(error){
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        setIsAuthenticated(await isAuth());
+      } catch (error) {
         console.error(error);
         setIsAuthenticated(false);
       }
     }
-  checkAuth();
+    checkAuth();
   }
-  ,[location])
+    , [location])
   return (
     <>
       <Routes>
         <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
         <Route path="/login" element={<FormPage isLogin={true} />} />
         <Route path="/signup" element={<FormPage isLogin={false} />} />
+        <Route path="/landing-page" element={<LandingPage />} />
       </Routes>
     </>
   );
